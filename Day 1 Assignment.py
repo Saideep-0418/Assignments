567f7c390dbbe71156dd8401fcb99e33c9b711cd
Saideep-0418 <saideep518@gmail.com>
2025-01-25 11:06:49 +0530

    Add files via upload

---
+++ Day 1 Assignment.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Declaring variables of different data types integer, float, string, and boolean
+my_integer = 10
+my_float = 3.14
+my_string = "Hello, Jupyter!"
+my_boolean = True
+print("Value of my_integer:", my_integer, " Type:", type(my_integer))
+print("Value of my_float:", my_float, " Type:", type(my_float))
+print("Value of my_string:", my_string, " Type:", type(my_string))
+print("Value of my_boolean:", my_boolean, " Type:", type(my_boolean))
+
